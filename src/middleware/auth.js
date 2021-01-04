bc361c22a503119212eb83c1252cfb4aa87ce2e1
unknown <maor30levy@gmail.com>
2021-01-04 14:18:20 +0200

    patch authentiation middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,7 +1,8 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
+const Costumer = require('../models/costumerModel');
 
-const auth = async (req, res, next) => {
+const userAuth = async (req, res, next) => {
     try {
         const token = req.header('Authorization').replace('Bearer ', '');
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
@@ -19,4 +20,22 @@ const auth = async (req, res, next) => {
     }
 };
 
-module.exports = auth;
+const costumerAuth = async (req, res, next) => {
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '');
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await Costumer.findOne({ _id: decoded._id, 'tokens.token': token });
+
+        if (!user) {
+            throw new Error();
+        }
+
+        req.user = user;
+        req.token = token;
+        next();
+    } catch (err) {
+        res.status(401).send({ error: 'Not authenticated' });
+    }
+};
+
+module.exports = {userAuth,costumerAuth};
